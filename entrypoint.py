d9e97ec8b7fb9e66dfa3a3990827ea0ab2a2df35
Ben Levinn <ben@levinn.dev>
2025-03-02 12:22:39 -0800

    Fix Python script to not require shell

--- entrypoint.py
+++ entrypoint.py
@@ -2,22 +2,22 @@ import os
 import subprocess
 
 def run_command(command):
-    result = subprocess.run(command, shell=True, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    result = subprocess.run(command, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     return result.stdout.decode('utf-8').strip()
 
 def main():
     bw_host = os.getenv('BW_HOST')
     bw_user = os.getenv('BW_USER')
 
-    run_command(f'bw config server {bw_host}')
+    run_command(['bw', 'config', 'server', bw_host])
 
-    bw_session = run_command(f'bw login {bw_user} --passwordenv BW_PASSWORD --raw')
+    bw_session = run_command(['bw', 'login', bw_user, '--passwordenv', 'BW_PASSWORD', '--raw'])
     os.environ['BW_SESSION'] = bw_session
 
-    run_command('bw unlock --check')
+    run_command(['bw', 'unlock', '--check'])
 
     print('Running `bw server` on port 8087')
-    run_command('bw serve --hostname 0.0.0.0')  # --disable-origin-protection
+    run_command(['bw', 'serve', '--hostname', '0.0.0.0'])  # --disable-origin-protection
 
 if __name__ == '__main__':
     main()
